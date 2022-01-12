35a2f96c30e21689bc5bd03884eb523c33eb9708
Raphael Monserate <55568600+RMonserate@users.noreply.github.com>
2022-01-11 17:57:54 -0800

    Create simplified controller class

---
+++ src/main/java/frc/controllers/Controller.java
@@ -0,0 +1,10 @@
+package frc.controllers;
+
+import edu.wpi.first.wpilibj.XboxController;
+
+public class Controller extends XboxController{
+    public Controller(int port)
+    {
+        super(port);
+    }
+}
