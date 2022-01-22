62d7ad57d0782a0c9f269292e6fcd8e51adc4b1f
Travis Cunningham <traviscunn94@gmail.com>
2022-01-22 12:54:52 -0800

    Apply spotless for java formatting

--- src/main/java/frc/controllers/BobTriggers.java
+++ src/main/java/frc/controllers/BobTriggers.java
@@ -6,27 +6,25 @@ forums.  The repo this was extracted from is https://github.com/team2097/BobBuil
 There is no obvious license file but want to retain pointers to them.
 
  */
+import edu.wpi.first.wpilibj.Joystick;
 import frc.controllers.BobXboxController.XboxAxis;
 
-import edu.wpi.first.wpilibj.Joystick;
-
-
 public class BobTriggers {
-	Joystick controller;
+    Joystick controller;
 
-	public BobTriggers(Joystick controller) {
-		this.controller = controller;
-	}
+    public BobTriggers(Joystick controller) {
+        this.controller = controller;
+    }
 
-	public double getLeft() {
-		return this.controller.getRawAxis(XboxAxis.LEFT_TRIGGER.value);
-	}
+    public double getLeft() {
+        return this.controller.getRawAxis(XboxAxis.LEFT_TRIGGER.value);
+    }
 
-	public double getRight() {
-		return this.controller.getRawAxis(XboxAxis.RIGHT_TRIGGER.value);
-	}
+    public double getRight() {
+        return this.controller.getRawAxis(XboxAxis.RIGHT_TRIGGER.value);
+    }
 
-	public double getTwist() {
-		return -getLeft() + getRight();
-	}
+    public double getTwist() {
+        return -getLeft() + getRight();
+    }
 }
