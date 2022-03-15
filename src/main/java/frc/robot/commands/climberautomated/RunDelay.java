7ae4b25ee49124d9655ba851e53df84f8e158611
Petty <4050009@stu.highlineschools.org>
2022-03-14 17:04:40 -0700

    made the new climber sequence for the redesign, also seperated the climber subsystem into climbermotor and climbersolenoids

--- src/main/java/frc/robot/commands/climberautomated/RunDelay.java
+++ src/main/java/frc/robot/commands/climberautomated/RunDelay.java
@@ -3,13 +3,13 @@ package frc.robot.commands.climberautomated;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ClimberSolenoids;
 
-public class Delay extends CommandBase {
+public class RunDelay extends CommandBase {
 
   private ClimberSolenoids climber;
   private double delay;
   private boolean done;
 
-  public Delay(ClimberSolenoids m_climber, double delayTime) {
+  public RunDelay(ClimberSolenoids m_climber, double delayTime) {
     climber = m_climber;
     delay = delayTime;
   }
--- src/main/java/frc/robot/commands/climberautomated/SeqClimberMain.java
+++ src/main/java/frc/robot/commands/climberautomated/SeqClimberMain.java
@@ -13,7 +13,7 @@ public class SeqClimberMain extends SequentialCommandGroup {
 
         // Moves arm to 6 bar
         new RunClimberArm(Robot.climberMotors, Robot.pid, 0.03 * 12, 0.01, 0.1),
-        new Delay(Robot.climberSolenoids, 0.5),
+        new RunDelay(Robot.climberSolenoids, 0.5),
 
         // Moves hooks off bar and rotate it 90 degrees, is a parallel command group
         new SeqRotateAndExtendPiston(),
@@ -21,7 +21,7 @@ public class SeqClimberMain extends SequentialCommandGroup {
         // Extends then rotates the hook onto 10 bar
         new RunPistonExtend(Robot.climberSolenoids, 1.5),
         new RunClimberArm(Robot.climberMotors, Robot.pid, 0.1944 * 12, 0.01, 0.1),
-        new Delay(Robot.climberSolenoids, 1),
+        new RunDelay(Robot.climberSolenoids, 1),
 
         // Releases arm from 6 bar
         new RunClimberArm(Robot.climberMotors, Robot.pid, 0 * 12, 0.005, 0.1),
@@ -34,7 +34,7 @@ public class SeqClimberMain extends SequentialCommandGroup {
 
         // Moves arm to 10 bar
         new RunClimberArm(Robot.climberMotors, Robot.pid, 0.03 * 12, 0.01, 0.1),
-        new Delay(Robot.climberSolenoids, 0.5),
+        new RunDelay(Robot.climberSolenoids, 0.5),
 
         // Moves hooks off bar and rotate it 90 degrees, is a parallel command group
         new SeqRotateAndExtendPiston(),
@@ -42,7 +42,7 @@ public class SeqClimberMain extends SequentialCommandGroup {
         // Extends then rotates the hook onto bar
         new RunPistonExtend(Robot.climberSolenoids, 1.5),
         new RunClimberArm(Robot.climberMotors, Robot.pid, 0.1944 * 12, 0.01, 0.1),
-        new Delay(Robot.climberSolenoids, 1),
+        new RunDelay(Robot.climberSolenoids, 1),
 
         // Releases arm from 10 bar
         new RunClimberArm(Robot.climberMotors, Robot.pid, 0 * 12, 0.01, 0.1),
